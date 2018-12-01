56444bf9d1e57f530849fb9652c0afa64039402a
Michael Kelly <L00137447@student.lyit.ie>
2018-12-01 20:32:23 +0000

    Final Loop.

--- DecisionsAndLoops/DecisionsAndLoops/Program.cs
+++ DecisionsAndLoops/DecisionsAndLoops/Program.cs
@@ -10,40 +10,13 @@ namespace DecisionsAndLoops
     {
         static void Main(string[] args)
         {
-            int score;
-            Console.WriteLine($"Enter your score");
-            string scoreAsText = Console.ReadLine();
-            score = Convert.ToInt16(scoreAsText);
-            if (score == 100)
+            int[] scores = new int[10];
+            foreach (var score in scores)
             {
-                // What is is in here, only gets executed 
-                //once the condition is met.
-                Console.WriteLine("Perfect score! You win");
-
+                Console.WriteLine($"Someone had this score : {score}");
             }
-            if (score >= 90)
-            {
-                Console.WriteLine("You got an A");
-            }
-            else if (score >=80)
-            {
-                Console.WriteLine("You got an B");
-            }
-            else if (score >= 70)
-            {
-                Console.WriteLine("You got an C");
-            }
-            else if (score >= 60)
-            {
-                Console.WriteLine("You got an D");
-            }
-            else
-            {
-                Console.WriteLine("you DIE!");
-            }
-           
-            // Everything here after if statement will still run.
             Console.ReadKey();
+            
         }
     }
 }
