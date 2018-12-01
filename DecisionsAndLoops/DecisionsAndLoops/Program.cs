3e660ee61f1af2eb8570c0b53d5092498c1896b1
Michael Kelly <L00137447@student.lyit.ie>
2018-12-01 17:47:19 +0000

    Decisions branch

--- DecisionsAndLoops/DecisionsAndLoops/Program.cs
+++ DecisionsAndLoops/DecisionsAndLoops/Program.cs
@@ -10,6 +10,40 @@ namespace DecisionsAndLoops
     {
         static void Main(string[] args)
         {
+            int score;
+            Console.WriteLine($"Enter your score");
+            string scoreAsText = Console.ReadLine();
+            score = Convert.ToInt16(scoreAsText);
+            if (score == 100)
+            {
+                // What is is in here, only gets executed 
+                //once the condition is met.
+                Console.WriteLine("Perfect score! You win");
+
+            }
+            if (score >= 90)
+            {
+                Console.WriteLine("You got an A");
+            }
+            else if (score >=80)
+            {
+                Console.WriteLine("You got an B");
+            }
+            else if (score >= 70)
+            {
+                Console.WriteLine("You got an C");
+            }
+            else if (score >= 60)
+            {
+                Console.WriteLine("You got an D");
+            }
+            else
+            {
+                Console.WriteLine("you DIE!");
+            }
+           
+            // Everything here after if statement will still run.
+            Console.ReadKey();
         }
     }
 }
